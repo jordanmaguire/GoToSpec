9025329374ec62b88654741ef00c1d358b25bfab
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2016-03-07 23:13:52 +0800

    Add some explanation of what is expected in variables

--- path_resolver.py
+++ path_resolver.py
@@ -2,6 +2,10 @@ import os
 
 class PathResolver:
 
+  # folder:     "/path/to/your/rails/app"
+  # dirname:    "/app/models"
+  # filename:   "user"
+  # extension:  ".rb"
   def find_verified_spec_path(self, folder, dirname, filename, extension):
     if dirname.startswith('/app'):
       dirname = dirname[4:]
@@ -13,6 +17,10 @@ class PathResolver:
     if os.path.isfile(spec_file):
       return spec_file
 
+  # folder:     "/path/to/your/rails/app"
+  # dirname:    "/app/models"
+  # filename:   "user"
+  # extension:  ".rb"
   def find_verified_implementation_path(self, folder, dirname, filename, extension):
     filename = filename[0:-5]
     dirname = dirname[5:] + '/'
@@ -21,6 +29,7 @@ class PathResolver:
     if os.path.isfile(test_subject):
       return test_subject
 
+    # spec/models/user_spec.rb -> app/models/user.rb
     dirname = '/app' + dirname
     test_subject = folder + dirname + filename + extension
     if os.path.isfile(test_subject):
